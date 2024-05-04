24b5ec717ea21f06e28c91d950819e0e3c5ddb25
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-05-04 16:01:21 +0100

    User Story 6 - Test 2: All Tests Passing

--- spec/StatementPrinter.spec.js
+++ spec/StatementPrinter.spec.js
@@ -18,7 +18,33 @@ describe("Statement Printer: ", () => {
 
             //Assert
             expect(console.log).toHaveBeenCalledWith(
-                "\ndate       || credit || debit || balance\n" + "14/01/2012 ||        || 50.00 || 250.00\n" + "13/01/2012 || 200.00 ||       || 300.00\n" + "10/01/2012 || 100.00 ||       || 100.00\n"
+                "\ndate       || credit || debit || balance\n" +
+                    "14/01/2012 ||        || 50.00 || 250.00\n" +
+                    "13/01/2012 || 200.00 ||       || 300.00\n" +
+                    "10/01/2012 || 100.00 ||       || 100.00\n"
+            );
+        });
+
+        it("Should print correctly when credit length longer than 6 characters", () => {
+            //Arrange
+            let testAccount = jasmine.createSpyObj("testAccount", {
+                getTransactions: [
+                    { date: "10/01/2012", amount: 1000 },
+                    { date: "13/01/2012", amount: 2000 },
+                    { date: "14/01/2012", amount: -50 },
+                ],
+            });
+            spyOn(console, "log");
+
+            //Act
+            StatementPrinter.printStatement(testAccount.getTransactions());
+
+            //Assert
+            expect(console.log).toHaveBeenCalledWith(
+                "\ndate       || credit  || debit || balance\n" +
+                    "14/01/2012 ||         || 50.00 || 2950.00\n" +
+                    "13/01/2012 || 2000.00 ||       || 3000.00\n" +
+                    "10/01/2012 || 1000.00 ||       || 1000.00\n"
             );
         });
     });
--- src/StatementPrinter.js
+++ src/StatementPrinter.js
@@ -1,25 +1,36 @@
 export default class StatementPrinter {
+    static #totalBalance;
+    static #widestCredit;
+    static #widestDebit;
+
     // Make the credit and debit section e.g.:
     // "100.00 ||        || "
     // or
     // "       || 100.00 || "
     static #constructCreditAndDebit(amount) {
         let amountString = Math.abs(amount).toFixed(2);
+
         let creditString = amount > 0 ? amountString : "";
         let debitString = amount > 0 ? "" : amountString;
+        this.#widestCredit = Math.max(creditString.length, this.#widestCredit);
+        this.#widestDebit = Math.max(debitString.length, this.#widestDebit);
 
-        return creditString.padEnd(6, " ") + " || " + debitString.padEnd(5, " ") + " || ";
+        return creditString.padEnd(this.#widestCredit, " ") + " || " + debitString.padEnd(this.#widestDebit, " ") + " || ";
+    }
+
+    static #constructEachLine(transaction) {
+        let line = transaction.date + " || ";
+        line += this.#constructCreditAndDebit(transaction.amount);
+        line += (this.#totalBalance += transaction.amount).toFixed(2);
+
+        return line;
     }
 
     static #constructTransactionLines(transactions) {
         let transactionLines = "";
-        let totalBalance = 0;
 
         for (const transaction of transactions) {
-            let line = transaction.date + " || ";
-            line += this.#constructCreditAndDebit(transaction.amount);
-            line += (totalBalance += transaction.amount).toFixed(2);
-
+            let line = this.#constructEachLine(transaction);
             transactionLines = line + "\n" + transactionLines;
         }
 
@@ -27,8 +38,18 @@ export default class StatementPrinter {
     }
 
     static printStatement(transactions) {
+        this.#totalBalance = 0;
+        this.#widestCredit = 6;
+        this.#widestDebit = 5;
+
         let toPrint = this.#constructTransactionLines(transactions);
-        toPrint = "\ndate       || credit || debit || balance\n" + toPrint;
+        toPrint =
+            "\ndate       || " +
+            "credit".padEnd(this.#widestCredit, " ") +
+            " || " +
+            "debit".padEnd(this.#widestDebit, " ") +
+            " || balance\n" +
+            toPrint;
 
         console.log(toPrint);
     }
